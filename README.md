e919fa3215b8036c12ee7244c57e6397516cc0de
npragin <npragin@gmail.com>
2026-01-30 18:02:50 -0800

    Initial commit.

---
+++ README.md
@@ -0,0 +1,5 @@
+# Floorplan Generator Stage
+
+Procedural floorplan generation and Stage simulation launcher. Generates office-style environments from TOML configurations and launches them in Stage.
+
+**Clone using:** `git clone --recurse-submodules git@github.com:npragin/floorplan-generator-stage.git`
---
+++ setup.py
@@ -0,0 +1,44 @@
+from setuptools import find_packages, setup
+
+package_name = "floorplan_generator_stage"
+
+setup(
+    name=package_name,
+    version="0.0.0",
+    packages=find_packages(exclude=["test"]) + ["floorplan_generator"],
+    package_dir={
+        "floorplan_generator": (
+            "floorplan_generator_stage/floorplan_generator/floorplan_generator"
+        ),
+    },
+    data_files=[
+        ("share/ament_index/resource_index/packages", ["resource/" + package_name]),
+        ("share/" + package_name, ["package.xml"]),
+        (
+            "share/" + package_name + "/config",
+            ["floorplan_generator_stage/floorplan_generator/config.toml"],
+        ),
+    ],
+    package_data={"": ["py.typed"]},
+    install_requires=[
+        "setuptools",
+        "shapely>=2.0.0",
+        "numpy>=1.26.0",
+        "pillow>=10.0.0",
+        "typer>=0.9.0",
+    ],
+    zip_safe=True,
+    maintainer="npragin",
+    maintainer_email="npragin@gmail.com",
+    description="Procedural floorplan generation and Stage simulation launcher. Generates office-style environments "
+    "from TOML configurations and launches them in Stage.",
+    license="GNU General Public License v3.0",
+    extras_require={
+        "test": [
+            "pytest",
+        ],
+    },
+    entry_points={
+        "console_scripts": [],
+    },
+)
